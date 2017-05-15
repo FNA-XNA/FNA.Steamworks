65ca91020afc066cde13c65f4fc3fcddce28af0f
Ethan Lee <flibitijibibo@gmail.com>
2017-05-14 23:46:05 -0400

    Just find the leaderboard without downloading, if needed

--- src/GamerServices/LeaderboardWriter.cs
+++ src/GamerServices/LeaderboardWriter.cs
@@ -7,10 +7,20 @@
  */
 #endregion
 
+#region Using Statements
+using Steamworks;
+#endregion
+
 namespace Microsoft.Xna.Framework.GamerServices
 {
 	public sealed class LeaderboardWriter
 	{
+		#region Private Variables
+
+		private string latestKey;
+
+		#endregion
+
 		#region Public Methods
 
 		public LeaderboardEntry GetLeaderboard(LeaderboardIdentity leaderboardId)
@@ -18,7 +28,24 @@ namespace Microsoft.Xna.Framework.GamerServices
 			// FIXME: Should we just magically have leaderboards by this point? -flibit
 			if (!LeaderboardReader.Leaderboards.ContainsKey(leaderboardId.Key))
 			{
-				LeaderboardReader.Read(leaderboardId, 0, 1);
+				latestKey = leaderboardId.Key;
+				SteamAPICall_t call = SteamUserStats.FindLeaderboard(leaderboardId.Key);
+				if (call.m_SteamAPICall != 0)
+				{
+					CallResult<LeaderboardFindResult_t> foundLeaderboard;
+					foundLeaderboard = new CallResult<LeaderboardFindResult_t>();
+					foundLeaderboard.Set(
+						call,
+						OnLeaderboardFound
+					);
+					while (GamerServicesDispatcher.UpdateAsync())
+					{
+						if (string.IsNullOrEmpty(latestKey))
+						{
+							break;
+						}
+					}
+				}
 			}
 
 			// FIXME: Do these other parameters even matter? -flibit
@@ -28,6 +55,24 @@ namespace Microsoft.Xna.Framework.GamerServices
 				0,
 				LeaderboardReader.Leaderboards[leaderboardId.Key]
 			);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void OnLeaderboardFound(
+			LeaderboardFindResult_t board,
+			bool bIOFailure
+		) {
+			if (!bIOFailure && board.m_bLeaderboardFound > 0)
+			{
+				LeaderboardReader.Leaderboards.Add(
+					latestKey,
+					board.m_hSteamLeaderboard
+				);
+			}
+			latestKey = null;
 		}
 
 		#endregion
